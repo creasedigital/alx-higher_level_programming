d08b2d6d320010728211df59ce8a8ea329fe468a
Ojore A. Chris <ojorechris@gmail.com>
2023-02-06 04:29:43 -0800

    8-edit

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle(BaseGeometry):
     """Represent a rectangle using BaseGeometry."""
 
     def __init__(self, width, height):
-        """Intialize a new Rectangle.
+        """__init__ method
         Args:
             width (int): The width of the new Rectangle.
             height (int): The height of the new Rectangle.
