cd85d016a6ba654d55124a64bf857ce0e34c5d33
Ojore A. Chris <ojorechris@gmail.com>
2023-02-06 07:18:54 -0800

    minor-fix

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-# 7-base_geometry.py
-"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """Reprsent base geometry."""
-
+    """BaseGeometry class with area method"""
     def area(self):
-        """Not yet implemented."""
+        """Area method not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
+        """integer validator
         Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
+            name (str): name
+            value (int): integer value
         Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
+            TypeError: if value is not an integer
+            ValueError: if value is less than 1
         """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+if __name__ == '__main__':
+    bg = BaseGeometry()
+
+    bg.integer_validator("my_int", 12)
+    bg.integer_validator("width", 89)
+
+    bg.area()
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,38 @@
 #!/usr/bin/python3
-# 8-rectangle.py
-"""Defines a class Rectangle that inherits from BaseGeometry."""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Represent a rectangle using BaseGeometry."""
+    """Rectangle class"""
 
     def __init__(self, width, height):
         """__init__ method
         Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
+            width (int): width integer must be greater than 0
+            height (int): height integer must be greater than 0
         """
         super().integer_validator("width", width)
         super().integer_validator("height", height)
         self.__width = width
-        self.__height = height 
+        self.__height = height
+
+
+if __name__ == '__main__':
+    r = Rectangle(3, 5)
+
+    print(r)
+    print(dir(r))
+
+    try:
+        print("Rectangle: {} - {}".format(r.width, r.height))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r2 = Rectangle(4, True)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    print(issubclass(Rectangle, BaseGeometry))
